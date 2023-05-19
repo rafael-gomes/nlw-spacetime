95deb2df67465ddc53f763e6066e7be392a5f89a
Rafael Gomes <rafaell.gomes@gmail.com>
2023-05-18 23:48:19 -0300

    Add jwt and authenticated routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,23 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 const app = fastify()
 
 app.register(cors, {
   origin: true,
   // origin: ['http://localhost:3000', 'http://rocketseat.com.br/'], // todas URLS, de front-end poderão acessar nosso back-end
 })
+
+app.register(jwt, {
+  secret: 'hdjkalhd1ljk2h3j12h3jkasdhjasldhhadjkhasldh12j3h',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
